8a0f0d4c80f5335a5b200e79b76b040db2b8751b
Maciej Sulej <187809@edu.p.lodz.pl>
2016-10-20 20:21:44 +0200

    initial commit

---
+++ src/main/java/pl/lab/znk/ZnkApplication.java
@@ -0,0 +1,12 @@
+package pl.lab.znk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZnkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZnkApplication.class, args);
+	}
+}
