9b06f1d5bb6d901d0eb62c0599e317c502c58ea6
Maciej Sulej <187809@edu.p.lodz.pl>
2016-10-20 21:04:34 +0200

    obsluga wyjatkow aplikacji

--- src/main/java/pl/lab/znk/exception/UserAlreadyExistsException.java
+++ src/main/java/pl/lab/znk/exception/UserAlreadyExistsException.java
@@ -3,7 +3,6 @@ package pl.lab.znk.exception;
 public class UserAlreadyExistsException extends AlreadyExistsException {
 
     private Caused caused;
-    private String identifier;
 
     public enum Caused {
         Login, Email
@@ -16,22 +15,17 @@ public class UserAlreadyExistsException extends AlreadyExistsException {
     public UserAlreadyExistsException(Caused caused, String identifier) {
         super("User " + identifier + " already exists");
         this.caused = caused;
-        this.identifier = identifier;
     }
 
     @Override
     public String getMessage() {
         switch (caused) {
             case Login:
-                return "User with login " + identifier + " already exists";
+                return "Login already exists";
             case Email:
-                return "User with email " + identifier + " already exists";
+                return "Email already exists";
         }
 
         return "";
-    }
-
-    public Caused getCaused() {
-        return caused;
     }
 }
