7e4dcb418030ea4cc3efe27e0c889bdc8a6933b6
Maciej Sulej <187809@edu.p.lodz.pl>
2016-11-08 20:35:14 +0100

    usuniecie liquibase

--- src/main/java/pl/lab/znk/config/CacheConfiguration.java
+++ src/main/java/pl/lab/znk/config/CacheConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.*;
-import org.springframework.cache.support.NoOpCacheManager; 
+import org.springframework.cache.support.NoOpCacheManager;
 
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
@@ -14,7 +14,7 @@ import javax.inject.Inject;
 @SuppressWarnings("unused")
 @Configuration
 @EnableCaching
-@AutoConfigureAfter(value = { MetricsConfiguration.class, DatabaseConfiguration.class })
+@AutoConfigureAfter(value = { MetricsConfiguration.class })
 public class CacheConfiguration {
 
     private final Logger log = LoggerFactory.getLogger(CacheConfiguration.class);
--- src/main/java/pl/lab/znk/config/liquibase/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Liquibase specific code.
- */
-package pl.lab.znk.config.liquibase;
