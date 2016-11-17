22396c038ed2544cfee79c734c5653f3a25373ad
Damian Bartos <damian483@gmail.com>
2016-11-17 01:42:49 +0100

    tworzenie konsultacji dodaje teraz aktualnie zalogowanego nauczyciela oraz ustawia domyslnie cancelled na false

--- src/main/webapp/app/entities/consultation/consultation-dialog.controller.js
+++ src/main/webapp/app/entities/consultation/consultation-dialog.controller.js
@@ -1,13 +1,13 @@
-(function() {
+(function () {
     'use strict';
 
     angular
         .module('znkApp')
         .controller('ConsultationDialogController', ConsultationDialogController);
 
-    ConsultationDialogController.$inject = ['$timeout', '$scope', '$stateParams', '$uibModalInstance', 'entity', 'Consultation', 'User'];
+    ConsultationDialogController.$inject = ['$timeout', '$scope', '$stateParams', '$uibModalInstance', 'entity', 'Consultation', 'User', 'Principal'];
 
-    function ConsultationDialogController ($timeout, $scope, $stateParams, $uibModalInstance, entity, Consultation, User) {
+    function ConsultationDialogController($timeout, $scope, $stateParams, $uibModalInstance, entity, Consultation, User, Principal) {
         var vm = this;
 
         vm.consultation = entity;
@@ -15,38 +15,68 @@
         vm.datePickerOpenStatus = {};
         vm.openCalendar = openCalendar;
         vm.save = save;
-        vm.users = User.query();
+        vm.users = [];
+        vm.currentUserId = -1;
 
-        $timeout(function (){
+        User.query({}, onSuccess);
+
+        function onSuccess(data, headers) {
+            //hide anonymous user from user management: it's a required user for Spring Security
+            for (var i in data) {
+                if (data[i]['login'] === 'anonymoususer') {
+                    data.splice(i, 1);
+                }
+            }
+            vm.users = data;
+            currentUser();
+        }
+
+        $timeout(function () {
             angular.element('.form-group:eq(1)>input').focus();
         });
 
-        function clear () {
+        function clear() {
             $uibModalInstance.dismiss('cancel');
         }
 
-        function save () {
+        function save() {
             vm.isSaving = true;
+            vm.consultation.cancelled = false;
+            vm.consultation.teacherId = vm.currentUserId;
             if (vm.consultation.id !== null) {
                 Consultation.update(vm.consultation, onSaveSuccess, onSaveError);
             } else {
+                console.log(vm.consultation);
                 Consultation.save(vm.consultation, onSaveSuccess, onSaveError);
             }
         }
 
-        function onSaveSuccess (result) {
+        function currentUser() {
+            Principal.identity().then(function(account) {
+                vm.account = account;
+                for (var i = 0; i < vm.users.length; i++) {
+                    console.log(vm.users[i].login);
+                    if (vm.users[i].login === vm.account.login) {
+                        vm.currentUserId = vm.users[i].id;
+                        break;
+                    }
+                }
+            });
+        }
+
+        function onSaveSuccess(result) {
             $scope.$emit('znkApp:consultationUpdate', result);
             $uibModalInstance.close(result);
             vm.isSaving = false;
         }
 
-        function onSaveError () {
+        function onSaveError() {
             vm.isSaving = false;
         }
 
         vm.datePickerOpenStatus.dateTime = false;
 
-        function openCalendar (date) {
+        function openCalendar(date) {
             vm.datePickerOpenStatus[date] = true;
         }
     }
