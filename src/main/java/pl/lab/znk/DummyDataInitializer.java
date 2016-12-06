332028e671ada97dba781ede79a5987aa3ea23b4
Maciej Sulej <187809@edu.p.lodz.pl>
2016-12-06 21:45:42 +0100

    dodanie przykladowych nauczycieli i studentow

---
+++ src/main/java/pl/lab/znk/DummyDataInitializer.java
@@ -0,0 +1,105 @@
+package pl.lab.znk;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import pl.lab.znk.domain.Authority;
+import pl.lab.znk.domain.Consultation;
+import pl.lab.znk.domain.User;
+import pl.lab.znk.repository.AuthorityRepository;
+import pl.lab.znk.repository.ConsultationRepository;
+import pl.lab.znk.repository.UserRepository;
+
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.HashSet;
+
+import static pl.lab.znk.security.AuthoritiesConstants.*;
+
+@Configuration
+public class DummyDataInitializer {
+
+    @Autowired
+    private AuthorityRepository authorityRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ConsultationRepository consultationRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Bean
+    public CommandLineRunner initApplicationData() {
+        return args -> {
+            initAuthorities();
+
+            User anon = createUser("anonymoususer", "anonymoususer", "anonymoususer@localhost", ANONYMOUS_AUTHORITY);
+            User system = createUser("system", "system", "system@localhost", ADMIN_AUTHORITY, USER_AUTHORITY);
+            User admin = createUser("admin", "admin", "admin@localhost", ADMIN_AUTHORITY, USER_AUTHORITY);
+            User user = createUser("user", "user", "user@localhost", USER_AUTHORITY);
+            User teacher = createUser("teacher", "teacher", "teacher@p.lodz.pl", "Anna", "Kowalska", USER_AUTHORITY, TEACHER_AUTHORITY);
+            User teacher2 = createUser("teacher2", "teacher2", "pnowak@p.lodz.pl", "Piotr", "Nowak", USER_AUTHORITY, TEACHER_AUTHORITY);
+            User teacher3 = createUser("teacher3", "teacher3", "mryniec@p.lodz.pl", "Marian", "Ryniec", USER_AUTHORITY, TEACHER_AUTHORITY);
+            User student = createUser("student", "student", "student@edu.p.lodz.pl", "Agata", "Wygnaniec", USER_AUTHORITY, STUDENT_AUTHORITY);
+            User student2 = createUser("student2", "student2", "tnowicki@edu.p.lodz.pl", "Tomasz", "Nowicki", USER_AUTHORITY, STUDENT_AUTHORITY);
+            User student3 = createUser("student3", "student3", "hkowalik@edu.p.lodz.pl", "Halina", "Kowalik", USER_AUTHORITY, STUDENT_AUTHORITY);
+
+            Arrays.asList(anon, system, admin, user, teacher, teacher2, teacher3, student, student2, student3)
+                .forEach(u -> userRepository.save(u));
+
+            Consultation consultation1 = createConsultation(teacher, "Przykladowy opis");
+            Consultation consultation2 = createConsultation(teacher2, "Inny przykladowy opis", 200, student, student2);
+            Consultation consultation3 = createConsultation(teacher2, "Jeszcze inny przykladowy opis", 10, student2);
+            Consultation consultation4 = createConsultation(teacher, "Informacja dla studentow", 321, student2, student3);
+            Consultation consultation5 = createConsultation(teacher3, "Calkiem inny opis", 241, student, student2, student3);
+
+            Arrays.asList(consultation1, consultation2, consultation3, consultation4, consultation5)
+            .forEach(c -> consultationRepository.save(c));
+        };
+    }
+
+    private void initAuthorities() {
+        Arrays.asList(ANONYMOUS_AUTHORITY, ADMIN_AUTHORITY, USER_AUTHORITY, TEACHER_AUTHORITY, STUDENT_AUTHORITY)
+            .forEach(authority ->
+                authorityRepository.save(authority)
+        );
+    }
+
+    private User createUser(String login, String password, String email, String firstName, String lastName,
+                            Authority ... authorities) {
+        User user = new User();
+        user.setAuthorities(new HashSet<>(Arrays.asList(authorities)));
+        user.setLogin(login);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setPassword(passwordEncoder.encode(password));
+        user.setEmail(email);
+        user.setActivated(true);
+        user.setCreatedDate(ZonedDateTime.now());
+        user.setCreatedBy("system");
+        return user;
+    }
+
+    private User createUser(String login, String password, String email, Authority ... authorities) {
+        return createUser(login, password, email, "", "", authorities);
+    }
+
+    private Consultation createConsultation(User teacher, String description, long hoursOffset, User ... students) {
+        Consultation consultation = new Consultation();
+        consultation.setTeacher(teacher);
+        consultation.setDescription(description);
+        consultation.setCancelled(false);
+        consultation.setRegisteredStudents(new HashSet<>(Arrays.asList(students)));
+        consultation.setDateTime(ZonedDateTime.now().plusHours(hoursOffset));
+        return consultation;
+    }
+
+    private Consultation createConsultation(User teacher, String description) {
+        return createConsultation(teacher, description, 0, new User[] {});
+    }
+}
--- src/main/java/pl/lab/znk/ZnkApp.java
+++ src/main/java/pl/lab/znk/ZnkApp.java
@@ -15,8 +15,6 @@ import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
-import pl.lab.znk.domain.Authority;
-import pl.lab.znk.domain.User;
 import pl.lab.znk.repository.AuthorityRepository;
 import pl.lab.znk.repository.UserRepository;
 
@@ -24,15 +22,11 @@ import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.time.ZonedDateTime;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashSet;
-
-import static pl.lab.znk.security.AuthoritiesConstants.*;
 
 @ComponentScan
-@EnableAutoConfiguration(exclude = { MetricFilterAutoConfiguration.class, MetricRepositoryAutoConfiguration.class })
+@EnableAutoConfiguration(exclude = { MetricFilterAutoConfiguration.class, MetricRepositoryAutoConfiguration.class, DummyDataInitializer.class })
 @EnableConfigurationProperties({ JHipsterProperties.class, LiquibaseProperties.class })
 public class ZnkApp {
 
@@ -40,6 +34,9 @@ public class ZnkApp {
 
     @Inject
     private Environment env;
+
+    @Inject
+    private DummyDataInitializer dataInitializer;
 
     /**
      * Initializes znk.
@@ -60,81 +57,6 @@ public class ZnkApp {
             log.error("You have misconfigured your application! It should not" +
                 "run with both the 'dev' and 'cloud' profiles at the same time.");
         }
-    }
-
-    @Bean
-    public CommandLineRunner initApplicationData(AuthorityRepository authorityRepository, UserRepository userRepository) {
-        return (args -> {
-            Authority anonAuth = new Authority(ANONYMOUS);
-            Authority adminAuth = new Authority(ADMIN);
-            Authority userAuth = new Authority(USER);
-            Authority teacherAuth = new Authority(TEACHER);
-            Authority studentAuth = new Authority(STUDENT);
-
-            Arrays.asList(anonAuth, adminAuth, userAuth, teacherAuth, studentAuth).forEach(authority ->
-                authorityRepository.save(authority)
-            );
-
-            User anon = new User();
-            anon.setAuthorities(new HashSet<>(Arrays.asList(anonAuth)));
-            anon.setLogin("anonymoususer");
-            anon.setPassword("$2a$10$mE.qmcV0mFU5NcKh73TZx.z4ueI/.bDWbj0T1BYyqP481kGGarKLG");
-            anon.setEmail("anonymoususer@localhost");
-            anon.setActivated(true);
-            anon.setCreatedDate(ZonedDateTime.now());
-            anon.setCreatedBy("system");
-            userRepository.save(anon);
-
-            User system = new User();
-            system.setAuthorities(new HashSet<>(Arrays.asList(adminAuth, userAuth)));
-            system.setLogin("system");
-            system.setPassword("$2a$10$mE.qmcV0mFU5NcKh73TZx.z4ueI/.bDWbj0T1BYyqP481kGGarKLG");
-            system.setEmail("system@localhost");
-            system.setActivated(true);
-            system.setCreatedDate(ZonedDateTime.now());
-            system.setCreatedBy("system");
-            userRepository.save(system);
-
-            User admin = new User();
-            admin.setAuthorities(new HashSet<>(Arrays.asList(adminAuth, userAuth)));
-            admin.setLogin("admin");
-            admin.setPassword("$2a$10$gSAhZrxMllrbgj/kkK9UceBPpChGWJA7SYIb1Mqo.n5aNLq1/oRrC");
-            admin.setEmail("admin@localhost");
-            admin.setActivated(true);
-            admin.setCreatedDate(ZonedDateTime.now());
-            admin.setCreatedBy("system");
-            userRepository.save(admin);
-
-            User user = new User();
-            user.setAuthorities(new HashSet<>(Arrays.asList(userAuth)));
-            user.setLogin("user");
-            user.setPassword("$2a$10$VEjxo0jq2YG9Rbk2HmX9S.k1uZBGYUHdUcid3g/vfiEl7lwWgOH/K");
-            user.setEmail("email@localhost");
-            user.setActivated(true);
-            user.setCreatedDate(ZonedDateTime.now());
-            user.setCreatedBy("system");
-            userRepository.save(user);
-
-            User teacher = new User();
-            teacher.setAuthorities(new HashSet<>(Arrays.asList(userAuth, teacherAuth)));
-            teacher.setLogin("teacher");
-            teacher.setPassword("$2a$10$TXxpw4xyNbbjZPGu0Qc1juDpHnIrTWte/S5i/qtvtITH7IUhX50Y6");
-            teacher.setEmail("teacher@p.lodz.pl");
-            teacher.setActivated(true);
-            teacher.setCreatedDate(ZonedDateTime.now());
-            teacher.setCreatedBy("system");
-            userRepository.save(teacher);
-
-            User student = new User();
-            student.setAuthorities(new HashSet<>(Arrays.asList(userAuth, studentAuth)));
-            student.setLogin("student");
-            student.setPassword("$2a$10$81A8zUpEgta4NvaG1Eqv5ua/WBp99vFxdVW8ky0A3MeaplcJVFSbC");
-            student.setEmail("student@edu.p.lodz.pl");
-            student.setActivated(true);
-            student.setCreatedDate(ZonedDateTime.now());
-            student.setCreatedBy("system");
-            userRepository.save(student);
-        });
     }
 
     /**
