d51f22ed8beb6795ab92e0d995990ec773152521
Maciej Sulej <187809@edu.p.lodz.pl>
2016-12-08 02:53:40 +0100

    uzycie repozytorium tokenow w serwisie

--- src/main/java/pl/lab/znk/service/util/FirebaseNotificationService.java
+++ src/main/java/pl/lab/znk/service/util/FirebaseNotificationService.java
@@ -7,9 +7,11 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.lab.znk.domain.Notification;
 import pl.lab.znk.domain.User;
+import pl.lab.znk.repository.UserTokenRepository;
 import pl.lab.znk.service.NotificationInterface;
 
 import java.io.IOException;
@@ -24,6 +26,13 @@ public class FirebaseNotificationService implements NotificationInterface {
     private static final String FIREBASE_SERVER_KEY = "key=AAAAWMmzNeY:APA91bFCnSXHObeJTGryvYW" +
         "qR7Nz-9t4LKIk8LW-NI3N_ddtXanauZkQ0EFh3jO5t0D_gXBXs4iB2NLs_H2E88CpXS5uUcYhJLR3bQZR" +
         "cmThvKMZyXiJ4Ks7ShW1hwp2crHaJTPq-mrvAV6Y6dchgIffVT5tKl6EhQ";
+
+    private UserTokenRepository userTokenRepository;
+
+    @Autowired
+    public FirebaseNotificationService(UserTokenRepository userTokenRepository) {
+        this.userTokenRepository = userTokenRepository;
+    }
 
     @Override
     public void notifyUsers(Collection<User> userCollection, Notification notification) {
@@ -85,6 +94,7 @@ public class FirebaseNotificationService implements NotificationInterface {
     }
 
     private String getUserToken(User user){
-        return "eH3mBZNWZ2w:APA91bHN7ILHpseLhkpNT_AJqyY1xkKCWhChg4tCMi2BfUyKdTND8Q0jm59ljup0MaggqI4Nc4iTu9drf62ENbxd9lNllWOotCQbLZeHRn8MA4GDeTix6Io7q0i1YdVjtO5gZHOPynYP";
+        return userTokenRepository.findByUser_id(user.getId()).getToken();
+//        return "eH3mBZNWZ2w:APA91bHN7ILHpseLhkpNT_AJqyY1xkKCWhChg4tCMi2BfUyKdTND8Q0jm59ljup0MaggqI4Nc4iTu9drf62ENbxd9lNllWOotCQbLZeHRn8MA4GDeTix6Io7q0i1YdVjtO5gZHOPynYP";
     }
 }
