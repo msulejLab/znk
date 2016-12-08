63a7fb8e2411bdb723070c7d77af142d2458351e
Maciej Sulej <187809@edu.p.lodz.pl>
2016-12-08 02:45:40 +0100

    przeksztalcenie FirebaseMessagingHelper w serwis

--- src/main/java/pl/lab/znk/service/util/FirebaseNotificationService.java
+++ src/main/java/pl/lab/znk/service/util/FirebaseNotificationService.java
@@ -7,55 +7,60 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 import pl.lab.znk.domain.Notification;
 import pl.lab.znk.domain.User;
+import pl.lab.znk.service.NotificationInterface;
 
 import java.io.IOException;
 import java.util.Collection;
 
-public class FirebaseMessagingHelper {
+@Service
+public class FirebaseNotificationService implements NotificationInterface {
 
-    private static final Logger log = LoggerFactory.getLogger(FirebaseMessagingHelper.class);
+    private static final Logger log = LoggerFactory.getLogger(FirebaseNotificationService.class);
 
     private static final String HOST_NAME = "https://fcm.googleapis.com/fcm/send";
     private static final String FIREBASE_SERVER_KEY = "key=AAAAWMmzNeY:APA91bFCnSXHObeJTGryvYW" +
         "qR7Nz-9t4LKIk8LW-NI3N_ddtXanauZkQ0EFh3jO5t0D_gXBXs4iB2NLs_H2E88CpXS5uUcYhJLR3bQZR" +
         "cmThvKMZyXiJ4Ks7ShW1hwp2crHaJTPq-mrvAV6Y6dchgIffVT5tKl6EhQ";
 
-    public static void notifyOne(User user, Notification notification){
+    @Override
+    public void notifyUsers(Collection<User> userCollection, Notification notification) {
+        for (User user : userCollection) {
+            notifyUser(user, notification);
+        }
+    }
+
+    @Override
+    public void notifyUser(User user, Notification notification) {
         String userToken = getUserToken(user);
         JsonObject bodyObject = makeRequestObject(notification, userToken);
         doHttpRequest(bodyObject);
     }
 
 
-    public static void notifyAll(Collection<User> users, Notification notification){
-        for (User user : users) {
-            notifyOne(user, notification);
-        }
-    }
-
-    private static JsonObject makeRequestObject(Notification notification, String userToken) {
+    private JsonObject makeRequestObject(Notification notification, String userToken) {
         JsonObject bodyObject = makeBody(userToken);
         JsonObject notificationObject = makeNotification(notification);
         bodyObject.add("notification", notificationObject);
         return bodyObject;
     }
 
-    private static JsonObject makeNotification(Notification notification) {
+    private JsonObject makeNotification(Notification notification) {
         JsonObject notificationObject = new JsonObject();
         notificationObject.addProperty("body", notification.getContent());
         notificationObject.addProperty("title", notification.getTitle());
         return notificationObject;
     }
 
-    private static JsonObject makeBody(String userToken){
+    private JsonObject makeBody(String userToken){
         JsonObject body = new JsonObject();
         body.addProperty("to", userToken);
         return body;
     }
 
-    private static void doHttpRequest(JsonObject bodyObject) {
+    private void doHttpRequest(JsonObject bodyObject) {
         CloseableHttpClient httpClient = HttpClientBuilder.create().build();
         try {
             HttpPost request = new HttpPost(HOST_NAME);
@@ -79,9 +84,7 @@ public class FirebaseMessagingHelper {
         }
     }
 
-    //tymczasowo zwracanie okreslonego do testow
-    //domyslnie pobieranie odpowiedniego tokenu z servisu
-    private static String getUserToken(User user){
+    private String getUserToken(User user){
         return "eH3mBZNWZ2w:APA91bHN7ILHpseLhkpNT_AJqyY1xkKCWhChg4tCMi2BfUyKdTND8Q0jm59ljup0MaggqI4Nc4iTu9drf62ENbxd9lNllWOotCQbLZeHRn8MA4GDeTix6Io7q0i1YdVjtO5gZHOPynYP";
     }
 }
