0bce08a0db956ff7be4dd8c817f1520cd972a796
Damian Bartos <damian483@gmail.com>
2016-12-08 21:12:45 +0100

    usuniecie security

--- src/main/java/pl/lab/znk/web/rest/UserResource.java
+++ src/main/java/pl/lab/znk/web/rest/UserResource.java
@@ -1,15 +1,6 @@
 package pl.lab.znk.web.rest;
 
-import pl.lab.znk.config.Constants;
 import com.codahale.metrics.annotation.Timed;
-import pl.lab.znk.domain.User;
-import pl.lab.znk.repository.UserRepository;
-import pl.lab.znk.security.AuthoritiesConstants;
-import pl.lab.znk.service.MailService;
-import pl.lab.znk.service.UserService;
-import pl.lab.znk.web.rest.vm.ManagedUserVM;
-import pl.lab.znk.web.rest.util.HeaderUtil;
-import pl.lab.znk.web.rest.util.PaginationUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
@@ -20,12 +11,22 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
+import pl.lab.znk.config.Constants;
+import pl.lab.znk.domain.User;
+import pl.lab.znk.repository.UserRepository;
+import pl.lab.znk.security.AuthoritiesConstants;
+import pl.lab.znk.service.MailService;
+import pl.lab.znk.service.UserService;
+import pl.lab.znk.web.rest.util.HeaderUtil;
+import pl.lab.znk.web.rest.util.PaginationUtil;
+import pl.lab.znk.web.rest.vm.ManagedUserVM;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
 import java.net.URISyntaxException;
-import javax.servlet.http.HttpServletRequest;
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -84,7 +85,7 @@ public class UserResource {
         method = RequestMethod.POST,
         produces = MediaType.APPLICATION_JSON_VALUE)
     @Timed
-    @Secured(AuthoritiesConstants.ADMIN)
+//    @Secured(AuthoritiesConstants.ADMIN)
     public ResponseEntity<?> createUser(@RequestBody ManagedUserVM managedUserVM, HttpServletRequest request) throws URISyntaxException {
         log.debug("REST request to save User : {}", managedUserVM);
 
@@ -124,16 +125,16 @@ public class UserResource {
         method = RequestMethod.PUT,
         produces = MediaType.APPLICATION_JSON_VALUE)
     @Timed
-    @Secured(AuthoritiesConstants.ADMIN)
+//    @Secured(AuthoritiesConstants.ADMIN)
     public ResponseEntity<ManagedUserVM> updateUser(@RequestBody ManagedUserVM managedUserVM) {
         log.debug("REST request to update User : {}", managedUserVM);
         Optional<User> existingUser = userRepository.findOneByEmail(managedUserVM.getEmail());
         if (existingUser.isPresent() && (!existingUser.get().getId().equals(managedUserVM.getId()))) {
-            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("userManagement", "emailexists", "E-mail already in use")).body(null);
+            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("userManagement", "emailexists", "E-mail already in use")).body(managedUserVM);
         }
         existingUser = userRepository.findOneByLogin(managedUserVM.getLogin().toLowerCase());
         if (existingUser.isPresent() && (!existingUser.get().getId().equals(managedUserVM.getId()))) {
-            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("userManagement", "userexists", "Login already in use")).body(null);
+            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("userManagement", "userexists", "Login already in use")).body(managedUserVM);
         }
         userService.updateUser(managedUserVM.getId(), managedUserVM.getLogin(), managedUserVM.getFirstName(),
             managedUserVM.getLastName(), managedUserVM.getEmail(), managedUserVM.isActivated(),
